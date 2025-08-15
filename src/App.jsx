1e2b8cb69713add72d365ed4d383ddb3252c7ba8
Adarsh <adarshtadiparthi30@gmail.com>
2025-08-15 19:27:43 +0530

    Intial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {BrowserRouter as Route,Routes,Router} from 'react-router-dom'
+import './App.css'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
